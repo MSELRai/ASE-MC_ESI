4f8a13eacb7f3ac3f8930082624599c22fc9eb5c
Woodrow N. Wilson <65731874+potus28@users.noreply.github.com>
2025-10-24 11:44:28 -0500

    Update install.sh for ASE path and library copy
    Refactor ASE path detection and MC library copying.

--- install.sh
+++ install.sh
@@ -4,15 +4,26 @@
 submit_dir=$(pwd)
 
 # Locate ASE
-ase_executable_path=$(which ase)
-cd "$(dirname "$ase_executable_path")"
-cd ..
-ase_path=$(pwd)
-echo "ASE Path is $(ase_path)"
+#ase_executable_path=$(which ase)
+#cd "$(dirname "$ase_executable_path")"
+#cd ..
+#ase_path=$(pwd)
+#echo "ASE Path is $(ase_path)"
 
 # Copy MC library
-echo "Copying MC library to $(pwd)/ase"
-cp -r $submit_dir/mc $(pwd)/ase
+#echo "Copying MC library to $(pwd)/ase"
+#cp -r $submit_dir/mc $(pwd)/ase
+#echo "Successfully added Monte Carlo to ASE!"
+
+# Locate ASE
+ase_path="$(python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')"
+cd "$ase_path/ase"
+echo "ASE Path is $(pwd)"
+
+# Copy MC library
+echo "Copying MC library to $(pwd)"
+cp -r $submit_dir/mc $(pwd)
+
 echo "Successfully added Monte Carlo to ASE!"
 
 # Done!
